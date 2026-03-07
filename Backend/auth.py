83e06ba17a43c74fc9d01a5f1c92487b6c4a2d63
JackDaWack <jackson.s.netter49@gmail.com>
2026-03-06 22:59:07 -0500

    -Implemented user retrieval.

--- Backend/auth.py
+++ Backend/auth.py
@@ -10,7 +10,14 @@ class User:
 
     def to_dict(self):
         return {"username" : self.username, "email" : self.email, "password" : self.password}
-    
+
+def retrieve_user(username: str):
+    db = main.database_connect()
+    user_data = db["users"].find_one({"username": username})
+    if user_data:
+        return User(user_data["username"], user_data["email"], user_data["password"])
+    return None
+
 @app.get("/login")
 def login():
     return
