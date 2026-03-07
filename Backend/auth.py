58aab8b02ac8db09c6456ab0249425e60a9879be
JackDaWack <jackson.s.netter49@gmail.com>
2026-03-06 22:48:46 -0500

    Revert "Odana was a quiet nation. It was the breadbasket of humanity. A kingdom of rolling farmland on plateaus nestled between mountain ranges. Though pieces of the past, bones and ruin still made themselves visible, clinging just above the surface like corpses of stone floating in lakes of golden wheat and barley."
    This reverts commit fd977d8ed858e20335a711e7ebc4e91d878769ba.

--- Backend/auth.py
+++ Backend/auth.py
@@ -2,22 +2,6 @@ import main
 
 app = main.app
 
-class User:
-    def __init__(self, username: str, email: str, password: str):
-        self.username = username
-        self.email = email
-        self.password = password
-    
-    def to_dict(self):        
-        return {"username": self.username,"email": self.email,"password": self.password}
-
-def retrieve_user(username: str):
-    db = main.database_connect()
-    user_data = db.users.find_one({"username": username})
-    if user_data:
-        return User(user_data["username"], user_data["email"], user_data["password"])
-    return None
-
 @app.get("/login")
 def login():
     return
