65e2b0bdb9f8b10d78ed7363bc5bf62d81fc73d6
JackDaWack <jackson.s.netter49@gmail.com>
2026-03-07 21:06:47 -0500

    -Added event listenters.

--- Frontend/auth.js
+++ Frontend/auth.js
@@ -2,3 +2,8 @@ async function login(username, password) {}
 
 async function register(username, email, password) {}
 
+const login_button = document.getElementById("login-button");
+const register_button = document.getElementById("register-button");
+
+login_button.addEventListener("click",login());
+register_button.addEventListener("click",register());
