c41320cc17123f9e65321b52a5943003920bbb84
JackDaWack <jackson.s.netter49@gmail.com>
2026-03-12 19:20:57 -0400

    -Edited error response in auth.js login function. -Implemented register function in auth.js. -Implemented Register_Data class in auth.py. -Implemented register function in auth.py.

--- Backend/auth.py
+++ Backend/auth.py
@@ -15,7 +15,11 @@ class User:
 class Login_Data(BaseModel):
     username: str
     password: str
-    
+
+class Register_Data(BaseModel):
+    username: str
+    email: str
+    password: str
 
 def retrieve_user(username: str):
     db = main.database_connect()
@@ -31,11 +35,11 @@ def login(data: Login_Data):
         return {"status": "success", "message": "User logged in successfully"}
     return {"status": "error", "message": "User login failed"}
 
-@app.get("/register")
-def register(username: str, email: str, password: str):
+@app.post("/register")
+def register(data: Register_Data):
     db = main.database_connect()
-    if db["users"].find_one({"username": username}):
-        return {"status": "error", "message": "Username already exists"}
-    user = User(username, email, password)
+    if db["users"].find_one({"username": data.username}):
+        return {"status": "error", "message": "User already exists"}
+    user  = User(data.username,data.email,data.password)
     db["users"].insert_one(user.to_dict())
-    return {"status": "success", "message": "User registered successfully"}
+    return {"status": "success", "message": "User registered in successfully"}
--- Frontend/auth.js
+++ Frontend/auth.js
@@ -4,9 +4,8 @@ async function login() {
         const password = document.getElementById("password").value;
         const response = await fetch("/login", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({username,password})})
         if (!response.ok) {
-            throw new Error("Login request failed");
+            throw new Error("User login request failed");
         }
-
         const data = await response.json();
         console.log("Server response:", data);
     }
@@ -14,10 +13,17 @@ async function login() {
 
 }
 
-async function register(username, email, password) {
+async function register() {
     try{
-        const response = await fetch("/register", {method: "POST"})
-
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const response = await fetch("/register", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({username,email,password})})
+        if (!response.ok) {
+            throw new Error("User registry request failed");
+        }
+        const data = await response.json();
+        console.log("Server response:", data);
     }
     catch(err){console.error("Error calling backend:", err);}
 }
