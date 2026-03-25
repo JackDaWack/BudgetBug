3c39b7cd857bed78aef13200b1c7c60f766a1ccf
JackDaWack <jackson.s.netter49@gmail.com>
2026-03-25 17:03:29 -0400

    -Changed default return value for login function in auth.py to redirect user back to login page upon an unsuccessful login.

--- Backend/auth.py
+++ Backend/auth.py
@@ -58,7 +58,7 @@ def login(data: Login_Data):
     #return {"status": "error", "message": "User login failed"}
     if get_user(data.username) and get_user(data.username)["password"] == data.password:
         return RedirectResponse(url="/", status_code=302)
-    return {"status": "error", "message": "User login failed"}
+    return RedirectResponse(url="/login-page", status_code=302)
 
 @router.post("/register")
 def register(data: Register_Data):
